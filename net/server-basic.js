1fb653c669ce879e3dc2e12bf096ada25f7ad954
sunsilently <sunsilently@outlook.com>
2020-10-04 23:08:27 +0800

    用telnet测试服务

--- net/server-basic.js
+++ net/server-basic.js
@@ -6,6 +6,9 @@ const PORT = 3000, HOST = 'localhost';
 // socket是一个事件发射器，可以使用on监听事件
 // 比如，监听事件，就使用 on('data', data => console.log(data))
 const server = net.createServer(socket => {
+    socket.on('data', data => {
+        socket.write(data);
+    });
     socket.end('goodbye');
 });
 
@@ -25,3 +28,8 @@ server.on('end', function () {
 });
 
 server.listen(PORT, HOST, () => console.log(`server is running at ${HOST}: ${PORT}`));
+
+// 可以用下面的命令运行服务
+// node server-basic.js
+
+// 通过 telnet 来模拟客户端，windows 系统需要安装 telnet 客户端程序
